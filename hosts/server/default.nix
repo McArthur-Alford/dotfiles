4656f7c3c625fbdc7f48e7fa809c24ab581cc392
mcarthur <mcarthur.alford@bigpond.com>
2023-05-16 06:51:50 +1000

    nvidia for server

--- hosts/server/default.nix
+++ hosts/server/default.nix
@@ -10,6 +10,14 @@
       ./hardware-configuration.nix
       ../../modules/desktop/hyprland 
       ../../modules/programs/wofi
+      ../../modules/programs/docker.nix
+      ../../modules/programs/python.nix
+      ../../modules/programs/haskell.nix
+      ../../modules/programs/zsh.nix
+      ../../modules/services/gnome-keyring.nix
+      ../../modules/kernels/latest.nix
+      ../../modules/gpu/nvidia.nix
+      # ../../modules/services/systemd-boot.nix
     ];
 
   # Use the GRUB 2 boot loader.
@@ -42,7 +50,6 @@
   # };
 
   services = {
-    blueman.enable = true;
     xserver = {
       enable = true;
 
@@ -65,74 +72,24 @@
     extraGroups = [ "networkmanager" "wheel" "audio" ];
   };
 
-  # Enable the GNOME Desktop Environment.
-  # services.xserver.displayManager.gdm.enable = true;
-  # services.xserver.desktopManager.gnome.enable = true;
-  
+  # auto usb mounting stuff
+  services.devmon.enable = true;
+  services.gvfs.enable = true;
+  services.udisks2.enable = true;
 
-  # Configure keymap in X11
-  # services.xserver.layout = "us";
-  # services.xserver.xkbOptions = {
-  #   "eurosign:e";
-  #   "caps:escape" # map caps to escape.
-  # };
+  networking = {
+    hostName = "nixos-server";
+    networkmanager.enable = true;
+  };
 
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
+  environment = {
+    systemPackages = with pkgs; [					# Packages not offered by Home-Manager
+      freshfetch
+      gnome.nautilus
+      nautilus-open-any-terminal
+      libsForQt5.qt5ct
+    ];
+  };
 
-  # Enable sound.
-  # sound.enable = true;
-  # hardware.pulseaudio.enable = true;
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  # users.users.alice = {
-  #   isNormalUser = true;
-  #   extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-  #   packages = with pkgs; [
-  #     firefox
-  #     thunderbird
-  #   ];
-  # };
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  # environment.systemPackages = with pkgs; [
-  #   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #   wget
-  # ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "22.11"; # Did you read the comment?
 }
