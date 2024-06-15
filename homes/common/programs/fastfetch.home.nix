76019695b0dd604581cde9d4c595ab754c8877ed
mcarthur <mcarthur.alford@proton.me>
2024-06-15 13:54:58 +0000

    fastfetch

---
+++ homes/common/programs/fastfetch.home.nix
@@ -0,0 +1,14 @@
+{}:
+{
+  programs.fastfetch = {
+    enable = true;
+    settings = {
+      logo = {
+        source = "nixos_small";
+        padding = {
+          right = 1;
+        };
+      };
+    };
+  };
+}
