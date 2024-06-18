aa5fdb241a8ac2138693d02bacf39fac468fd927
mcarthur <mcarthur.alford@proton.me>
2024-06-18 04:14:53 +0000

    fastfetch

--- homes/common/desktop/alucard.nix
+++ homes/common/desktop/alucard.nix
@@ -5,9 +5,9 @@
     ../programs/wofi.home.nix
     ../console/zellij.home.nix
     ../console/zsh.home.nix
-    # ../services/eww.home.nix
+    ../services/eww.home.nix
     # ../services/waybar.home.nix
-    ../services/ags.home.nix
+    # ../services/ags.home.nix
     ../programs/kitty.home.nix
     ../services/swaylock.home.nix
   ];
@@ -18,6 +18,9 @@
     };
     zathura = {
       enable = true;
+    };
+    pywal = {
+      # enable = true;
     };
   };
 
--- homes/common/programs/fastfetch.home.nix
+++ homes/common/programs/fastfetch.home.nix
@@ -1,14 +1,44 @@
-{}:
+{ ... }:
 {
   programs.fastfetch = {
     enable = true;
     settings = {
-      logo = {
-        source = "nixos_small";
-        padding = {
-          right = 1;
-        };
+      display = {
+        binaryPrefix = "si";
+        color = "blue";
+        separator = "  ";
       };
+      modules = [
+        "title"
+        "separator"
+        "os"
+        "host"
+        "kernel"
+        "uptime"
+        "packages"
+        "shell"
+        "display"
+        "de"
+        "wm"
+        "wmtheme"
+        "theme"
+        "icons"
+        "font"
+        "cursor"
+        "terminal"
+        "terminalfont"
+        "cpu"
+        "gpu"
+        "memory"
+        "swap"
+        "disk"
+        "localip"
+        "battery"
+        "poweradapter"
+        "locale"
+        "break"
+        "colors"
+      ];
     };
   };
 }
