12f172bcf7c25ef5b379ec193dfb6772e17ca0fe
mcarthur <mcarthur.alford@proton.me>
2024-07-08 14:41:40 +0000

    fixed cache

--- nixos/common/programs/caching.nix
+++ nixos/common/programs/caching.nix
@@ -14,17 +14,17 @@
   };
 
   nix.settings = {
+    netrc-file = config.sops.secrets.netrc-file.path;
     substituters = [
       "https://mmwave.cachix.org"
       "https://cache.nixos.org/"
       "https://nix-community.cachix.org"
-      "https://attic.thaumaturgy.tech"
+      "https://attic.thaumaturgy.tech/mcarthur"
     ];
     trusted-public-keys = [
       "mmwave.cachix.org-1:51WVqkk3jgt8S5rmsTZVsFvPw06FpTd1niyrFzJ6ucQ="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "mcarthur:1gfD7pB8Ozu910iBrQHV91bW33yaujeF6dfgJpS38PY="
     ];
-    netrc-file = config.sops.secrets.netrc-file.path;
   };
 }
